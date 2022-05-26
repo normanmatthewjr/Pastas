b58d800e0faa9fe692e052e452c93ce752c740ae
Norman Matthew, Jr <nmatthewjr@outlook.com>
2022-05-26 15:26:37 -0700

    add js to index and change css

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let userName = prompt("What is your name?");
 console.log(userName);
 document.write("Hello " + userName + ", welcome to our favorite Italian pastas page.");
+
+
+function myFunction() {
+  confirm("Confirm Answer");
+}
+
