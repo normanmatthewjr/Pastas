c143c476f506829717bb4c3d9b3435c98ddbe7db
Norman Matthew, Jr <nmatthewjr@outlook.com>
2022-05-27 22:26:59 -0700

    added rating function js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-let userName = prompt("What is your name?");
-console.log(userName);
-document.write("Hello " + userName);
+ let userName = prompt("What is your name?");
+ console.log(userName);
+ document.write("Hello " + userName );
+
+ function ratePage(){
+  let userNumber = prompt("How many stars would you rate my page? 1-5");
+  for (let i = 1; i <= userNumber; i++){
+    document.write("<img class='star-pic' src='star.jpg' />");;
+  }
+}
 
 function myFunction() {
-  
-  confirm("Click OK if your positive POSITIVE");
+  confirm("Click OK if you're a fan of Italian pastas");
   
 }
 
