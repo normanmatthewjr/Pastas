27ed3d86a2df123f4ef97876eeeaeab2b8712741
Norman Matthew, Jr <nmatthewjr@outlook.com>
2022-05-27 23:53:42 -0700

    labs turn in

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
- let userName = prompt("What is your name?");
- console.log(userName);
- document.write("Hello " + userName );
+function yourName() {
+  let name;
+      name = prompt('What is your name?');
+      console.log(name);
+  return document.write("Hello" + name);
+}
 
  function ratePage(){
   let userNumber = prompt("How many stars would you rate my page? 1-5");
   for (let i = 1; i <= userNumber; i++){
-    document.write("<img class='star-pic' src='star.jpg' />");;
+    document.write("<img class='star-pic' src='star.jpg' />");
   }
 }
 
-function myFunction() {
-  confirm("Click OK if you're a fan of Italian pastas");
-  
+
+function getPasta() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+    alert("I see you like" + "demo");
 }
+
+function myFan() {
+  confirm("Click OK if you're a fan of Italian pastas");
+  }
 
