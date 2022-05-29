caa431de1f3e7794c84b5844790b53c7ef39970a
Norman Matthew, Jr <nmatthewjr@outlook.com>
2022-05-29 14:40:38 -0700

    finish lab 7 if else

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 let userName = "";
+let fanText = "";
 
 function yourName() {
- 
       userName = prompt('What is your name?');
       console.log(userName);
   return document.write("Hello " + userName);
 }
 
  function ratePage(){
-  let userNumber = prompt("How many stars would you rate my page? 1-5");
-  for (let i = 1; i <= userNumber; i++){
-    document.write("<img class='star-pic' src='star.jpg' />");
+  let userRating = prompt("How many stars would you rate my page? Only 1-5");
+  let message = "";
+ 
+  if (userRating == 5) {
+    message = "WOW 5 star?!, Thank you " + userName + " you are AWESOME! Not going to stop improving though.";
+    alert(message);
+  }
+  else if (userRating == 4) {
+    message = "GREAT! 4 stars for a beginner? Thank you " + userName + "; there is still more to learn";
+    alert(message);
+  }
+  else if (userRating == 3) {
+    message = "AWESOME! 3 stars mean I'm doing something right but still a lot opportunities to grow.";
+    alert(message);
+  }  
+  else if (userRating == 2) {
+    message = "Only 2 stars? PERFECT! It means there are more exciting things to learn.";
+    alert(message);
+  }  
+  else if (userRating == 1) {
+    message = "Excellent! Going UP for here forward. Just need a little help, different strategy, and determination. Thank you.";
+    alert (message);
+  }   
+  else {
+    message = "Not a valid entry";
+    alert(message);
+  }
+
+  for (let rating = 1; rating <= userRating; rating++) {
+  document.write("<img class='star-pic' src='star.jpg' />");
   }
 }
 
 function getPasta() {
     var x = document.getElementById("myText").value;
     document.getElementById("demo").innerHTML = x;
-    alert("I see you like");
+    alert("I see you like" + x);
 }
 
 function myFan() {
-  let fanText;
   if (confirm("Click OK for more pastas") == true) {
     fanText = "Congratulations " + userName + ", you are ONE OF US!";
   }
@@ -30,4 +56,3 @@ function myFan() {
   }
   document.getElementById("pastaFanText").innerHTML = fanText;
 }
-
